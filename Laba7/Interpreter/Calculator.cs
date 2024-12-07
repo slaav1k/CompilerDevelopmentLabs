1c2194f65335045fabf3a18960082aadf009ef5d
Vyacheslav <74726728+slaav1k@users.noreply.github.com>
2024-12-07 11:49:56 +0300

    Add files via upload

---
+++ Laba7/Interpreter/Calculator.cs
@@ -0,0 +1,176 @@
+﻿using ClassLiberty.Entities;
+using ClassLiberty.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Interpreter
+{
+  internal class Calculator
+  {
+    private SymbolsDic symbolsDic;
+    private List<Instruction> instructions;
+
+    public Calculator(SymbolsDic symbolsDic, List<Instruction> instructions)
+    {
+      this.symbolsDic = symbolsDic;
+      this.instructions = instructions;
+    }
+
+    public void ExecuteInstructions()
+    {
+      foreach (var instruction in instructions)
+      {
+        // Если инструкция содержит результат (например, результат присваивания или операции)
+        var resultToken = instruction.ResToken;
+
+        // Определяем операнды и выполняем операцию
+        if (instruction.Tokens.Count == 2) // Для бинарных операций (например, сложение, вычитание)
+        {
+          var operand1 = instruction.Tokens[0];
+          var operand2 = instruction.Tokens[1];
+
+          // Получаем значения операндов из символов
+          double value1 = GetValue(operand1);
+          double value2 = GetValue(operand2);
+
+          // Получаем типы операндов из таблицы символов
+          var type1 = operand1.Type;
+
+          var type2 = operand2.Type;
+
+          if (operand1.Type == LexicalTypesEnum.IntegerConstant || operand1.Type == LexicalTypesEnum.RealConstant)
+          {
+            type1 = LexicalTypesEnum.IntegerConstant == operand1.Type ? LexicalTypesEnum.IntType : LexicalTypesEnum.FloatType;
+          } else
+          {
+            type1 = symbolsDic[operand1.IdentifierID].Type;
+          }
+
+          if (operand2.Type == LexicalTypesEnum.IntegerConstant || operand2.Type == LexicalTypesEnum.RealConstant)
+          {
+            type2 = LexicalTypesEnum.IntegerConstant == operand2.Type ? LexicalTypesEnum.IntType : LexicalTypesEnum.FloatType;
+          }
+          else
+          {
+            type2 = symbolsDic[operand2.IdentifierID].Type;
+          }
+
+          double result = 0;
+
+          // Выполняем операцию в зависимости от типа инструкции
+          switch (instruction.Manual)
+          {
+            case GenerationInstructionsEnum.add:
+              result = value1 + value2;
+              break;
+            case GenerationInstructionsEnum.sub:
+              result = value1 - value2;
+              break;
+            case GenerationInstructionsEnum.mul:
+              result = value1 * value2;
+              break;
+            case GenerationInstructionsEnum.div:
+              result = value1 / value2;
+              break;
+            default:
+              Console.WriteLine("Неизвестная операция");
+              break;
+          }
+
+          // Устанавливаем тип результата
+          LexicalTypesEnum resultType = DetermineResultType(type1, type2);
+          symbolsDic[resultToken.IdentifierID].Value = result;
+          symbolsDic[resultToken.IdentifierID].Type = resultType;
+
+          //Console.WriteLine($"Результат операции {instruction.Manual}: {result}, тип результата: {resultType}");
+          Console.WriteLine($"Результат операции {instruction.Manual}: {Math.Round(result, 4)} ; тип результата: {resultType}");
+        }
+        else if (instruction.Tokens.Count == 1) // Для унарных операций (например, отрицание)
+        {
+          var operand = instruction.Tokens[0];
+
+          // Получаем значение операнда
+          double value = GetValue(operand);
+
+          // Получаем тип операнда из таблицы символов
+
+          var operandType = operand.Type;
+
+          if (operand.Type == LexicalTypesEnum.IntegerConstant || operand.Type == LexicalTypesEnum.RealConstant)
+          {
+            operandType = LexicalTypesEnum.IntegerConstant == operand.Type ? LexicalTypesEnum.IntType : LexicalTypesEnum.FloatType;
+          }
+          else
+          {
+            operandType = symbolsDic[operand.IdentifierID].Type;
+          }
+
+          double result = 0;
+
+          // Выполняем унарную операцию
+          switch (instruction.Manual)
+          {
+            case GenerationInstructionsEnum.i2f: // Преобразование целого в вещественное число
+              result = value; // Преобразуем как нужно
+              break;
+            default:
+              Console.WriteLine("Неизвестная унарная операция");
+              break;
+          }
+
+          // Устанавливаем тип результата
+          LexicalTypesEnum resultType = operandType == LexicalTypesEnum.IntType
+              ? LexicalTypesEnum.FloatType
+              : operandType;
+
+          symbolsDic[resultToken.IdentifierID].Value = result;
+          symbolsDic[resultToken.IdentifierID].Type = resultType;
+
+          Console.WriteLine($"Результат операции {instruction.Manual}: {Math.Round(result, 4)} ; тип результата: {resultType}");
+          //Console.WriteLine($"Результат операции {instruction.Manual}: " + string.Format("{0:0.0000}", result) + $" тип результата: {resultType}");
+        }
+      }
+    }
+
+    // Метод для получения значения токена, может быть переменной или константой
+    private double GetValue(Token token)
+    {
+      if (token.Type == LexicalTypesEnum.IntegerConstant || token.Type == LexicalTypesEnum.RealConstant)
+      {
+        return Convert.ToDouble(token.Value);
+      } 
+      else
+      {
+        var symbol = symbolsDic[token.IdentifierID];
+
+        // Получаем значение в зависимости от типа символа
+        if (symbol.Type == LexicalTypesEnum.IntType || symbol.Type == LexicalTypesEnum.FloatType)
+        {
+          return Convert.ToDouble(symbol.Value);
+        }
+        else
+        {
+          // Для других типов данных возвращаем значение как строку или выполняем дополнительную обработку
+          return Convert.ToDouble(symbol.Value);
+        }
+      }
+      
+    }
+
+    // Метод для определения типа результата в зависимости от типов операндов
+    private LexicalTypesEnum DetermineResultType(LexicalTypesEnum type1, LexicalTypesEnum type2)
+    {
+      // Логика для определения типа результата, в зависимости от типов операндов
+      if (type1 == LexicalTypesEnum.IntType && type2 == LexicalTypesEnum.IntType)
+      {
+        return LexicalTypesEnum.IntType;
+      }
+      else
+      {
+        // Если хотя бы один операнд — вещественное число, результат будет вещественным
+        return LexicalTypesEnum.FloatType;
+      }
+    }
+  }
+}
---
+++ Laba7/Interpreter/RunnerInterpreteter.cs
@@ -0,0 +1,116 @@
+﻿using ClassLiberty.Enums;
+using ClassLiberty.Entities;
+
+namespace Interpreter
+{
+  internal class RunnerInterpreteter
+  {
+    static void Main(string[] args)
+    {
+      SymbolsDic deserializedSymbolsDic;
+      List<Instruction> deserializedInstructions;
+
+      string combinedFilePath = "../../../../CompLab7/bin/Debug/net8.0/post_code.bin";
+      using (FileStream fs = new FileStream(combinedFilePath, FileMode.Open))
+      using (BinaryReader reader = new BinaryReader(fs))
+      {
+        // Десериализация SymbolsDic
+        deserializedSymbolsDic = SymbolsDic.Deserialize(reader);
+        Console.WriteLine("Deserialized SymbolsDic:");
+        foreach (var pair in deserializedSymbolsDic.GetSymbols())
+        {
+          Console.WriteLine($"{pair.Key}: {pair.Value}");
+        }
+
+        // Десериализация списка инструкций
+        int instructionCount = reader.ReadInt32(); // Читаем количество инструкций
+        deserializedInstructions = new List<Instruction>();
+        for (int i = 0; i < instructionCount; i++)
+        {
+          deserializedInstructions.Add(Instruction.Deserialize(reader)); // Десериализуем каждую инструкцию
+        }
+
+        Console.WriteLine("Deserialized List of Instructions:");
+        foreach (var instruction in deserializedInstructions)
+        {
+          Console.WriteLine(instruction);
+        }
+      }
+
+
+
+      foreach (var symbol in deserializedSymbolsDic.GetSymbols())
+      {
+        // Пропускаем временные переменные (имя начинается с #T)
+        if (!symbol.Value.Name.StartsWith("#T"))
+        {
+          bool validInput = false;
+          while (!validInput)
+          {
+            Console.WriteLine($"Введите значение для переменной {symbol.Value.Name} (тип: {symbol.Value.Type}):");
+            string input = Console.ReadLine();
+
+            try
+            {
+              // Преобразуем ввод в нужный тип
+              if (symbol.Value.Type == LexicalTypesEnum.IntType)
+              {
+                if (int.TryParse(input, out int intValue))
+                {
+                  symbol.Value.Value = intValue;
+                  validInput = true;  // Ввод корректен, выходим из цикла
+                }
+                else
+                {
+                  Console.WriteLine("Некорректный ввод! Пожалуйста, введите целое число.");
+                }
+              }
+              else if (symbol.Value.Type == LexicalTypesEnum.FloatType || symbol.Value.Type == LexicalTypesEnum.RealConstant)
+              {
+                if (double.TryParse(input, out double doubleValue))
+                {
+                  symbol.Value.Value = doubleValue;
+                  validInput = true;  
+                }
+                else
+                {
+                  Console.WriteLine("Некорректный ввод! Пожалуйста, введите число с плавающей точкой.");
+                }
+              }
+              else
+              {
+                symbol.Value.Value = input; 
+                validInput = true; 
+              }
+            }
+            catch (Exception ex)
+            {
+              Console.WriteLine($"Ошибка: {ex.Message}. Попробуйте снова.");
+            }
+          }
+
+          Console.WriteLine($"Для {symbol.Value.Name} установлено значение: {symbol.Value.Value} (тип: {symbol.Value.Value.GetType().Name})");
+        }
+      }
+
+      //// Пример для подсчета суммы, предполагая, что все значения теперь числа:
+      //double sum = 0;
+      //foreach (var symbol in deserializedSymbolsDic.GetSymbols())
+      //{
+      //  if (!symbol.Value.Name.StartsWith("#T"))
+      //  {
+      //    sum += Convert.ToDouble(symbol.Value.Value); // Конвертируем в double для подсчета суммы
+      //  }
+      //}
+
+      //Console.WriteLine($"Сумма всех значений: {sum}");
+
+      Calculator calculator = new Calculator(deserializedSymbolsDic, deserializedInstructions);
+
+      calculator.ExecuteInstructions();
+
+
+
+    }
+  }
+}
